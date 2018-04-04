bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Remove direct bitcoin calls from qt/clientmodel.cpp

src/qt/bitcoin.cpp
@@ -455,7 +455,7 @@ void BitcoinApplication::initializeResult(bool success)
         paymentServer->setOptionsModel(optionsModel);
 #endif
 
-        clientModel = new ClientModel(optionsModel);
+        clientModel = new ClientModel(ipcNode, optionsModel);
         window->setClientModel(clientModel);
 
 #ifdef ENABLE_WALLET

Mattras007/bxcoin

src/qt/bitcoin.cpp
@@ -460,7 +460,7 @@ void BitcoinApplication::initializeResult(bool success)
         paymentServer->setOptionsModel(optionsModel);
 #endif
 
-        clientModel = new ClientModel(optionsModel);
+        clientModel = new ClientModel(m_node, optionsModel);
         window->setClientModel(clientModel);
 
 #ifdef ENABLE_WALLET