bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

GUI/Intro: Return actual prune setting from showIfNeeded

src/qt/bitcoin.h
@@ -68,7 +68,7 @@ public:
     /// Create options model
     void createOptionsModel(bool resetSettings);
     /// Initialize prune setting
-    void InitializePruneSetting(bool prune);
+    void InitPruneSetting(int64_t prune_opt);
     /// Create main window
     void createWindow(const NetworkStyle *networkStyle);
     /// Create splash screen

Mattras007/bxcoin

src/qt/bitcoin.h
@@ -68,7 +68,7 @@ public:
     /// Create options model
     void createOptionsModel(bool resetSettings);
     /// Initialize prune setting
-    void InitializePruneSetting(bool prune);
+    void InitPruneSetting(int64_t prune_MiB);
     /// Create main window
     void createWindow(const NetworkStyle *networkStyle);
     /// Create splash screen